7a7c86e1952d1e87cf75024d9e4f2c3051a63e86
Ronald <selsery@gmail.com>
2013-04-25 12:34:17 +0200

    Adding registers to netcode, first step

--- lib/netcode.rb
+++ lib/netcode.rb
@@ -1,9 +1,10 @@
 class Netcode
-  attr_accessor :connection
+  attr_accessor :connection, :registers
 
   def initialize
     #setup_connection
     #start_feed_loop
+    self.registers = []
   end
 
   def connected?
