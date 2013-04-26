587aa83a4132ed43edb32c4b9372253992fa3a91
Ronald <selsery@gmail.com>
2013-04-26 11:16:44 +0200

    Adds priority level of registers, where 0 is default and limited to max 2

--- lib/netcode.rb
+++ lib/netcode.rb
@@ -4,14 +4,23 @@ class Netcode
   def initialize
     #setup_connection
     #start_feed_loop
-    self.registers = []
+    self.registers = [[], [], []]
+  end
+
+  def add_register(register, prio = 0)
+    raise Netcode::RegisterPriorityToHighError if prio > 2
+    self.registers[prio] << register
+  end
+
+  def priority(prio_level)
+    registers[prio_level]
   end
 
   def connected?
     !connection.nil?
   end
 
-  def fetch_info
-    registers.map(&:fetch)
+  def fetch_info(prio = 0)
+    priority(prio).map(&:fetch)
   end
 end
--- spec/integration/adding_registers_to_netcode_spec.rb
+++ spec/integration/adding_registers_to_netcode_spec.rb
@@ -8,17 +8,33 @@ describe Netcode do
   end
 
   it "allows adding registers to a netcode" do
-    subject.registers << register
-    subject.registers.count.should == 1
+    subject.add_register(register)
+    subject.priority(0).count.should == 1
   end
 
   context ".send_thru" do
     before do
-      subject.registers << register
+      subject.add_register(register)
     end
 
     it "fetches the registers information to be send through" do
       subject.fetch_info.should include(object.send(method))
+    end
+
+    context ".priority" do
+      let(:high_prio_object)  { double("high_prio") }
+
+      let(:high_prio_register){ Register.new(high_prio_object, :position, 2) }
+
+      before do
+        high_prio_object.stub(:position) { "high prio info" }
+      end
+
+      it "only fetches of the priority asked" do
+        subject.add_register(high_prio_register, 2)
+
+        subject.fetch_info(2).should == ["high prio info"]
+      end
     end
   end
 end
